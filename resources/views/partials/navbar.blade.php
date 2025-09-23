fa01149547465fd033bcbaf1e7d0a7a9d1be95e3
astinurin <asthienoerin@gmail.com>
2025-09-23 15:50:49 +0700

    masih add template

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -14,14 +14,17 @@
       </a>
     </div>
   </div>
+
   <div class="navbar-menu-wrapper d-flex align-items-top">
     <ul class="navbar-nav">
       <li class="nav-item font-weight-semibold d-none d-lg-block ms-0">
         <h1 class="welcome-text">Good Morning, <span class="text-black fw-bold">John Doe</span></h1>
-        <h3 class="welcome-sub-text">Your performance summary this week </h3>
+        <h3 class="welcome-sub-text">Your performance summary this week</h3>
       </li>
     </ul>
+
     <ul class="navbar-nav ms-auto">
+      {{-- Dropdown kategori --}}
       <li class="nav-item dropdown d-none d-lg-block">
         <a class="nav-link dropdown-bordered dropdown-toggle dropdown-toggle-split" id="messageDropdown" href="#"
           data-bs-toggle="dropdown" aria-expanded="false"> Select Category </a>
@@ -33,7 +36,7 @@
           <div class="dropdown-divider"></div>
           <a class="dropdown-item preview-item">
             <div class="preview-item-content flex-grow py-2">
-              <p class="preview-subject ellipsis font-weight-medium text-dark">Bootstrap Bundle </p>
+              <p class="preview-subject ellipsis font-weight-medium text-dark">Bootstrap Bundle</p>
               <p class="fw-light small-text mb-0">This is a Bundle featuring 16 unique dashboards</p>
             </div>
           </a>
@@ -57,6 +60,8 @@
           </a>
         </div>
       </li>
+
+      {{-- Datepicker --}}
       <li class="nav-item d-none d-lg-block">
         <div id="datepicker-popup" class="input-group date datepicker navbar-date-picker">
           <span class="input-group-addon input-group-prepend border-right">
@@ -65,12 +70,16 @@
           <input type="text" class="form-control">
         </div>
       </li>
+
+      {{-- Search --}}
       <li class="nav-item">
         <form class="search-form" action="#">
           <i class="icon-search"></i>
           <input type="search" class="form-control" placeholder="Search Here" title="Search here">
         </form>
       </li>
+
+      {{-- Notifikasi --}}
       <li class="nav-item dropdown">
         <a class="nav-link count-indicator" id="notificationDropdown" href="#" data-bs-toggle="dropdown">
           <i class="icon-bell"></i>
@@ -79,7 +88,7 @@
         <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list pb-0"
           aria-labelledby="notificationDropdown">
           <a class="dropdown-item py-3 border-bottom">
-            <p class="mb-0 font-weight-medium float-left">You have 4 new notifications </p>
+            <p class="mb-0 font-weight-medium float-left">You have 4 new notifications</p>
             <span class="badge badge-pill badge-primary float-right">View all</span>
           </a>
           <a class="dropdown-item preview-item py-3">
@@ -88,7 +97,7 @@
             </div>
             <div class="preview-item-content">
               <h6 class="preview-subject fw-normal text-dark mb-1">Application Error</h6>
-              <p class="fw-light small-text mb-0"> Just now </p>
+              <p class="fw-light small-text mb-0">Just now</p>
             </div>
           </a>
           <a class="dropdown-item preview-item py-3">
@@ -97,7 +106,7 @@
             </div>
             <div class="preview-item-content">
               <h6 class="preview-subject fw-normal text-dark mb-1">Settings</h6>
-              <p class="fw-light small-text mb-0"> Private message </p>
+              <p class="fw-light small-text mb-0">Private message</p>
             </div>
           </a>
           <a class="dropdown-item preview-item py-3">
@@ -106,11 +115,13 @@
             </div>
             <div class="preview-item-content">
               <h6 class="preview-subject fw-normal text-dark mb-1">New user registration</h6>
-              <p class="fw-light small-text mb-0"> 2 days ago </p>
+              <p class="fw-light small-text mb-0">2 days ago</p>
             </div>
           </a>
         </div>
       </li>
+
+      {{-- Email --}}
       <li class="nav-item dropdown">
         <a class="nav-link count-indicator" id="countDropdown" href="#" data-bs-toggle="dropdown" aria-expanded="false">
           <i class="icon-mail icon-lg"></i>
@@ -118,42 +129,45 @@
         <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list pb-0"
           aria-labelledby="countDropdown">
           <a class="dropdown-item py-3">
-            <p class="mb-0 font-weight-medium float-left">You have 7 unread mails </p>
+            <p class="mb-0 font-weight-medium float-left">You have 7 unread mails</p>
             <span class="badge badge-pill badge-primary float-right">View all</span>
           </a>
           <div class="dropdown-divider"></div>
-          <a class="dropdown-item preview-item py-3">
+          <a class="dropdown-item preview-item">
             <div class="preview-thumbnail">
               <img src="{{ asset('assets/images/faces/face10.jpg') }}" alt="image" class="img-sm profile-pic">
             </div>
             <div class="preview-item-content flex-grow py-2">
-              <p class="preview-subject ellipsis font-weight-medium text-dark">Marian Garner </p>
-              <p class="fw-light small-text mb-0"> The meeting is cancelled </p>
+              <p class="preview-subject ellipsis font-weight-medium text-dark">Marian Garner</p>
+              <p class="fw-light small-text mb-0">The meeting is cancelled</p>
             </div>
           </a>
-          <a class="dropdown-item preview-item py-3">
+          <a class="dropdown-item preview-item">
             <div class="preview-thumbnail">
               <img src="{{ asset('assets/images/faces/face12.jpg') }}" alt="image" class="img-sm profile-pic">
             </div>
             <div class="preview-item-content flex-grow py-2">
-              <p class="preview-subject ellipsis font-weight-medium text-dark">David Grey </p>
-              <p class="fw-light small-text mb-0"> The meeting is cancelled </p>
+              <p class="preview-subject ellipsis font-weight-medium text-dark">David Grey</p>
+              <p class="fw-light small-text mb-0">The meeting is cancelled</p>
             </div>
           </a>
-          <a class="dropdown-item preview-item py-3">
+          <a class="dropdown-item preview-item">
             <div class="preview-thumbnail">
               <img src="{{ asset('assets/images/faces/face1.jpg') }}" alt="image" class="img-sm profile-pic">
             </div>
             <div class="preview-item-content flex-grow py-2">
-              <p class="preview-subject ellipsis font-weight-medium text-dark">Travis Jenkins </p>
-              <p class="fw-light small-text mb-0"> The meeting is cancelled </p>
+              <p class="preview-subject ellipsis font-weight-medium text-dark">Travis Jenkins</p>
+              <p class="fw-light small-text mb-0">The meeting is cancelled</p>
             </div>
           </a>
         </div>
       </li>
+
+      {{-- User dropdown --}}
       <li class="nav-item dropdown d-none d-lg-block user-dropdown">
         <a class="nav-link" id="UserDropdown" href="#" data-bs-toggle="dropdown" aria-expanded="false">
-          <img class="img-xs rounded-circle" src="{{ asset('assets/images/faces/face8.jpg') }}" alt="Profile image"> </a>
+          <img class="img-xs rounded-circle" src="{{ asset('assets/images/faces/face8.jpg') }}" alt="Profile image">
+        </a>
         <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="UserDropdown">
           <div class="dropdown-header text-center">
             <img class="img-md rounded-circle" src="{{ asset('assets/images/faces/face8.jpg') }}" alt="Profile image">
@@ -172,4 +186,11 @@
         </div>
       </li>
     </ul>
-    <button class="navbar-toggler navbar
+
+    {{-- Toggle offcanvas menu (mobile) --}}
+    <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button"
+      data-bs-toggle="offcanvas">
+      <span class="mdi mdi-menu"></span>
+    </button>
+  </div>
+</nav>
